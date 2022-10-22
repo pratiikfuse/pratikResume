2750337fd4c1a74368baf75326bd4ac635953ba7
pratiikfuse <pratikfuse20@gmail.com>
2022-10-22 18:24:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Personal Resume
+
+Resume built using plane HTML,CSS,Javascript.
+
+You can use butttons at the navigation section to directly access
+particular section in Resume.
+
+Contribute to this if you can .
+  
+
+
+## Author
+
+- [@pratiikfuse](https://github.com/pratiikfuse)
+
+
+## Lessons Learned
+
+Working of HTML,CSS,Javascript. Some basic stuffs how to use tags , CSS styles and javascript
+integration with HTML\CSS.
---
+++ script.js
@@ -0,0 +1,55 @@
+function highLightDiv(value)
+{
+    ;
+    idno = value.id;
+
+    if(idno ==='1')
+    {
+        document.getElementById("qualification").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("qualification").style.backgroundColor="white";
+        },1000);
+    }
+    else if(idno === '2')
+    {
+        document.getElementById("links").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("links").style.backgroundColor="white";
+        },1000);
+    
+    }
+    else if(idno === '3')
+    {
+        document.getElementById("skills").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("skills").style.backgroundColor="white";
+        },1000);
+        
+    }
+    else if(idno === '4')
+    {
+        document.getElementById("personal_details").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("personal_details").style.backgroundColor="white";
+        },1000);
+        
+    }
+    else if(idno === '5')
+    {
+        document.getElementById("media").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("media").style.backgroundColor="white";
+        },1000);
+    }
+    else if(idno === '6')
+    {
+        document.getElementById("certificate").style.backgroundColor="yellow";
+        setTimeout(()=>{
+            document.getElementById("certificate").style.backgroundColor="white";
+        },1000);
+    }
+   
+ 
+   
+}
+
